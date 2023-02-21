fc79fd7113e719509b12c8fabc3dd295e76dc8d7
klasing1159 <klasing1159@gmail.com>
2023-02-22 00:22:34 +0100

    bla

---
+++ Mixer_Fmnt_26-01-2023_v1/Resource.h
@@ -0,0 +1,39 @@
+#define IDC_LVOLUME_CHNL1		30400
+#define IDC_RVOLUME_CHNL1		30401
+#define IDC_LVOLUME_CHNL2		30402
+#define IDC_RVOLUME_CHNL2		30403
+#define IDC_LVOLUME_CHNL3		30404
+#define IDC_RVOLUME_CHNL3		30405
+#define IDC_LVOLUME_OUT 		30406
+#define IDC_RVOLUME_OUT 		30407
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Mixer_Fmnt_26-01-2023_v1.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_MIXERFMNT26012023V1_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_MIXERFMNT26012023V1			107
+#define IDI_SMALL				108
+#define IDC_MIXERFMNT26012023V1			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
